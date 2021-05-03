ecdcb651aedaf57dd89a53b25565452cb2fbac8d
Vitali <kentkentkent28@gmail.com>
2021-05-03 11:39:20 +0300

    extract serilog configuration to appsettsings.json

--- HotelReservation/Api/Program.cs
+++ HotelReservation/Api/Program.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
@@ -9,9 +11,18 @@ namespace HotelReservation.Api
         public static void Main(string[] args)
         {
             ConfigureLogger();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
             try
             {
+                Log.Information("Starting up");
                 CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
             }
             finally
             {
@@ -29,7 +40,12 @@ namespace HotelReservation.Api
 
         public static void ConfigureLogger()
         {
-            Log.Logger = new LoggerConfiguration().WriteTo.File(@"log.txt").CreateLogger();
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
         }
     }
 }
