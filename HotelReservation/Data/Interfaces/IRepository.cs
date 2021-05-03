b4c6ab8b3e97016b8cf32f894fad5768ebe1eec5
Vitali <kentkentkent28@gmail.com>
2021-05-03 11:42:31 +0300

    interface segregation

--- HotelReservation/Data/Interfaces/IRepository.cs
+++ HotelReservation/Data/Interfaces/IRepository.cs
@@ -1,22 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace HotelReservation.Data.Interfaces
 {
    public interface IRepository<T> where T : class
     {
         IEnumerable<T> GetAll();
-        Task<IEnumerable<T>> GetAllAsync();
         T Get(int id);
-        Task<T> GetAsync(int id);
         IEnumerable<T> Find(Func<T, Boolean> predicate);
-        Task<IEnumerable<T>> FindAsync(Func<T, bool> predicate);
         void Create(T item);
-        Task CreateAsync(T item);
         void Update(T item);
-        Task UpdateAsync(T newItem);
         void Delete(int id);
-        Task DeleteAsync(int id);
     }
 }
